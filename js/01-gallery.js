0d5dcac755ea96fc34f7672403596090ad3a656c
Artur Myasnikov <102307516+Artur7707@users.noreply.github.com>
2022-07-05 20:53:28 +0300

    hw-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -50,3 +50,19 @@ const instance = basicLightbox.create(`
       alt=""
     />
 `);
+
+// const gallery = document.querySelector(".gallery");
+// const galleryItemsRef = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<div class = "gallery__item"><a class = "gallery__link" href= "${original}"><img  class = "gallery__image" data-source = "${original}" src = ${preview} alt = "${description}"></a></div>`;
+//   })
+//   .join("");
+
+// gallery.insertAdjacentHTML("beforeend", galleryItemsRef);
+
+// // взяли из https://simplelightbox.com/ (раздел Usage)
+// const lightbox = new SimpleLightbox(".gallery a", {
+//   captionsPosition: "bottom",
+//   captionsData: "alt",
+//   captionsDelay: 250,
+// });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsRef = galleryItems.map(
+  ({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  }
+);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItemsRef.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsPosition: "bottom",
+  captionsDelay: 250,
+});
